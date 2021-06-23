18305ca7d532471d072c0718bb07abf4598a4c98
gorsobes <gorsobes@yandex.ru>
2021-06-23 22:18:25 +0600

    componenta

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return <header className='header'>
+    <img src="https://e7.pngegg.com/pngimages/1024/68/png-clipart-lego-logo-icons-logos-emojis-iconic-brands.png" alt="" />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return  (
+    <div className='content'>
+    <div>
+     <img src="https://s1.1zoom.ru/big0/697/Love_Night_Moon_Trees_Silhouette_Two_Dating_576752_1280x853.jpg" alt="" />
+     </div>
+   <div>
+    ava +
+   </div>
+  <div>
+my post
+  </div>
+  <div>
+new post
+  </div>
+  <div>
+post 1
+  </div>
+  <div>
+post 2
+</div>
+    </div>
+    ); 
+}
+
+export default Profile;

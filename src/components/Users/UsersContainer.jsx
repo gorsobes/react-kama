0a672196ca93419b8f2d0fada2047ec5d3ac98dc
gorsobes <gorsobes@yandex.ru>
2021-11-14 17:53:06 +0600

    ref_thunkiddleware

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { follow,unfollow,setUsers,setCurrentPage,setTotalUsersCount,toggleIsFetching,togglefollowingInProgress} from '../../redux/users-reducer';
+import { follow,unfollow,setCurrentPage,togglefollowingInProgress,getUsers} from '../../redux/users-reducer';
 import Users from './Users';
 import Preloader from '../common/Preloader/Preloader';
-import {usersAPI} from "../../api/api";
 
 class UsersAPIComponent extends React.Component {
    
     componentDidMount(){
-        this.props.toggleIsFetching(true);
+      this.props.getUsers(this.props.currentPage, this.props.pageSize);
+      
+      /*  this.props.toggleIsFetching(true);
 
         usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                     this.props.toggleIsFetching(false);
                     this.props.setUsers(data.items);
                     this.props.setTotalUsersCount(data.totalCount);
-                });
+                });*/
     }
       
                 
     onPageChanged = (pageNumber) => {
-        this.props.toggleIsFetching(true);
-        this.props.setCurrentPage(pageNumber);
-
-        //axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{ withCredentials:true})
-        usersAPI.getUsers(pageNumber, this.props.pageSize).then(data => {
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(data.items);
-         
-        });
-        
-        
+        this.props.getUsers(pageNumber, this.props.pageSize);
+           
     }
     
-
- 
-    
-
-
     render() {
 
         return <>
@@ -72,11 +59,9 @@ let mapStateToProps = (state) => {
 let mapDispachToProps =  {
             follow,
             unfollow,
-            setUsers,
             setCurrentPage,
-            setTotalUsersCount,
-            toggleIsFetching,
-            togglefollowingInProgress
+            togglefollowingInProgress,
+            getUsers
             }
 
 
